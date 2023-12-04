2fdb1e6d021da4c3cfba794cf787f72b7a3013f9
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-12-04 21:33:13 +0100

    updated project display and some other text

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -10,12 +10,9 @@ function ProjectDisplay() {
   return (
     <div className="project">
       <h1>{project.name}</h1>
-      <img src={project.image} />
+      <img src={project.image} alt={project.imageDesc} />
       <p>
-        <b>Languages:</b> {project.languages}
-      </p>
-      <p>
-        <b>Technologies:</b> {project.techologies}
+        <b>Skills:</b> {project.skills}
       </p>
       <GitHubIcon />
     </div>
