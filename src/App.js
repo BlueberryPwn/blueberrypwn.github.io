23e2e143042ac035bae42900749ab7ffc9b4ce0d
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-12-04 14:45:24 +0100

    added pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Projects />} />
+          <Route path="/" element={<Experience />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
