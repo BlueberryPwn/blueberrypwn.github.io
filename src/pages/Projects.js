56ac4fc3b0cf1050aa9b1bd3a190b968dd57e671
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-12-04 19:07:10 +0100

    created projectitem component & added base structure to projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,16 @@
 import React from "react";
+import ProjectItem from "../components/ProjectItem";
 
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <h1>My Projects</h1>
+      <div className="projectList">
+        <ProjectItem />
+        <ProjectItem />
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
