61ca8ee42588c227795ab1511858f5eb7f175969
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-12-04 19:51:22 +0100

    created assets folder and added images to project, added css to projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,16 @@
+import "../styles/Projects.css";
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
+import Proj1 from "../assets/proj1.png";
+import Proj2 from "../assets/proj2.png";
 
 function Projects() {
   return (
     <div className="projects">
       <h1>My Projects</h1>
       <div className="projectList">
-        <ProjectItem />
-        <ProjectItem />
+        <ProjectItem name="Project 1" image={Proj1} />
+        <ProjectItem name="Project 2" image={Proj2} />
       </div>
     </div>
   );
