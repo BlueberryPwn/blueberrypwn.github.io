4bd125c92235d9062286eeb6c3082b967bd359cc
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-12-04 20:02:49 +0100

    added helper for projects page

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,33 @@
+import Proj1 from "../assets/proj1.png";
+import Proj2 from "../assets/proj2.png";
+import Proj3 from "../assets/proj3.png";
+import Proj4 from "../assets/proj4.png";
+import Proj5 from "../assets/proj5.png";
+import Proj6 from "../assets/proj6.png";
+
+export const ProjectList = [
+  {
+    name: "Anonymous forum",
+    image: Proj1,
+  },
+  {
+    name: "Movie database",
+    image: Proj2,
+  },
+  {
+    name: "Car race",
+    image: Proj3,
+  },
+  {
+    name: "Bank application",
+    image: Proj4,
+  },
+  {
+    name: "Minimal SQL application",
+    image: Proj5,
+  },
+  {
+    name: "Number Guessing Game",
+    image: Proj6,
+  },
+];
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,16 +1,16 @@
 import "../styles/Projects.css";
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
-import Proj1 from "../assets/proj1.png";
-import Proj2 from "../assets/proj2.png";
+import { ProjectList } from "../helpers/ProjectList";
 
 function Projects() {
   return (
     <div className="projects">
       <h1>My Projects</h1>
       <div className="projectList">
-        <ProjectItem name="Project 1" image={Proj1} />
-        <ProjectItem name="Project 2" image={Proj2} />
+        {ProjectList.map((project) => {
+          return <ProjectItem name={project.name} image={project.image} />;
+        })}
       </div>
     </div>
   );
